5e1d2c63f6b4abc77d963178d36baf5db48b9db2
Victor Koome <koomev786@gmail.com>
2024-08-23 15:15:44 +0300

    add reducer to handle log in and log out

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -1,15 +1,55 @@
-import { createContext, useContext } from "react";
+import { act, createContext, useContext } from "react";
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
-function AuthProvider({children}) {
-   return <AuthContext.Provider>
-    {children}
-   </AuthContext.Provider>
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        user: action.payload,
+        isAuthenticated: true,
+      };
+    case "LOGOUT":
+      return initialState;
+    default:
+      throw new Error("Invalid action type");
+  }
+}
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  function login(email, password) {
+    if (email === FAKE_USER.name && password === FAKE_USER.password) {
+      dispatch({ type: "LOGIN", payload: FAKE_USER });
+    }
+  }
+  function logout() {
+    dispatch({ type: "LOGOUT" });
+  }
+
+  return <AuthContext.Provider>{children}</AuthContext.Provider>;
 }
 
 function useAuth() {
-    const context = useContext(AuthContext);
-    if (!context) throw new Error("useAuth must be used within an AuthProvider");
-    return context;
+  const context = useContext(AuthContext);
+  if (!context) throw new Error("useAuth must be used within an AuthProvider");
+  return context;
 }
+
+//export them so that they can be available for use
+
+export { AuthProvider, useAuth };
